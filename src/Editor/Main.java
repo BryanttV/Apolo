bffa3266ccb3c1be0e0ea766f52596fb81ca5e6b
Luciano <chavarria890lc@gmail.com>
2021-04-15 12:35:57 -0500

    NA

--- src/Editor/Main.java
+++ src/Editor/Main.java
@@ -2,6 +2,6 @@ package editor;
 import java.io.*;
 public class Main {
 	public static void main(String[] args) throws Exception{
-		System.out.println("Hello World");
+		System.out.println("Hello mundo");
 	}
 }
